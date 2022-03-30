c49c60052db7577d68dec02bba1987eda3fcccc7
PhongGT <megayako9@gmail.com>
2022-03-30 08:39:42 +0700

    Xử lý giờ, phút lấy dữ liệu

--- app/src/main/java/com/example/alarmclock/AlarmFragment.java
+++ app/src/main/java/com/example/alarmclock/AlarmFragment.java
@@ -1,5 +1,7 @@
 package com.example.alarmclock;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.app.Dialog;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +30,12 @@ public class AlarmFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    Button btn_them;
+    Dialog myDialog;
+    Button btn_Huy;
+    Button btn_Ok;
+    TimePicker timePicker;
+    Calendar calendar;
 
     public AlarmFragment() {
         // Required empty public constructor
@@ -53,12 +66,46 @@ public class AlarmFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm, container, false);
+        View v = inflater.inflate(R.layout.fragment_alarm, container, false);
+        myDialog = new Dialog(this.getContext());
+        btn_them = v.findViewById(R.id.Add_btn);
+        btn_them.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                myDialog.setContentView(R.layout.new_alarm_popup);
+                btn_Huy =(Button) myDialog.findViewById(R.id.close_btn);
+                timePicker = myDialog.findViewById(R.id.TimePikerAlarm);
+                timePicker.setIs24HourView(true);
+                btn_Ok = (Button) myDialog.findViewById(R.id.confirm_btn);
+                calendar = Calendar.getInstance();
+                btn_Huy.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        myDialog.dismiss();
+                    }
+                });
+                btn_Ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        calendar.set(Calendar.HOUR_OF_DAY, timePicker.getCurrentHour());
+                        calendar.set(Calendar.MINUTE, timePicker.getCurrentMinute());
+                        int Hour_set = timePicker.getCurrentHour();
+                        int Minute_set = timePicker.getCurrentMinute();
+                        btn_them.setText(String.valueOf(Hour_set) + String.valueOf(Minute_set));
+                    }
+                });
+                myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                myDialog.show();
+            }
+        });
+        return v;
     }
 }
--- app/src/main/java/com/example/alarmclock/MainActivity.java
+++ app/src/main/java/com/example/alarmclock/MainActivity.java
@@ -4,24 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
-
+import android.app.Dialog;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    Dialog myDialog;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         viewPager = (ViewPager) findViewById(R.id.view_pager);
-
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+        myDialog = new Dialog(this);
     }
 }
