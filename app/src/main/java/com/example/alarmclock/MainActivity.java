a7519517b332ad0cf24d1a54c6e40ba737d8be5c
Cao Van Tan <minhtan006237@gmail.com>
2022-04-04 15:17:19 +0700

    fig bug

--- app/src/main/java/com/example/alarmclock/MainActivity.java
+++ app/src/main/java/com/example/alarmclock/MainActivity.java
@@ -20,14 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
-    ListView listView;
-    TextView txt1;
-    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
