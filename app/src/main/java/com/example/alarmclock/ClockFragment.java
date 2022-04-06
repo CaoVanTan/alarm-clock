559bb0154bf5b40b1a995adaeeed2aeca1ba463a
Cao Van Tan <minhtan006237@gmail.com>
2022-04-06 10:14:00 +0700

    small change

--- app/src/main/java/com/example/alarmclock/ClockFragment.java
+++ app/src/main/java/com/example/alarmclock/ClockFragment.java
@@ -50,6 +50,7 @@ public class ClockFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_clock, container, false);
+
         btnAddClock = view.findViewById(R.id.btnAddClock);
         lvClock = view.findViewById(R.id.lvClock);
         db = new ClockDbHelper(getContext());
